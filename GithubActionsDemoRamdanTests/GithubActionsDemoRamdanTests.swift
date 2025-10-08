081bb146e1c8a9965e7457f8ac2bad64ef9ea696
Ramdan <dadanisme@upi.edu>
2025-10-08 13:47:28 +0800

    add mock unit test

--- GithubActionsDemoRamdanTests/GithubActionsDemoRamdanTests.swift
+++ GithubActionsDemoRamdanTests/GithubActionsDemoRamdanTests.swift
@@ -24,6 +24,8 @@ final class GithubActionsDemoRamdanTests: XCTestCase {
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        XCTAssertEqual(1 + 1, 2)
+        XCTAssertNil(nil)
     }
 
     func testPerformanceExample() throws {
