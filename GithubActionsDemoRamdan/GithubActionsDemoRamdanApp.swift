dfe24ac211467d68e5bdca2458e034752617fbb2
Ramdan <dadanisme@upi.edu>
2025-10-08 13:44:29 +0800

    Initial Commit

---
+++ GithubActionsDemoRamdan/GithubActionsDemoRamdanApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubActionsDemoRamdanApp.swift
+//  GithubActionsDemoRamdan
+//
+//  Created by Ramdan on 08/10/25.
+//
+
+import SwiftUI
+
+@main
+struct GithubActionsDemoRamdanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
